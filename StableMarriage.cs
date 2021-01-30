ef26f538791754fb2f27965764f4f7eb76490c87
Abukasis <AddieAbuka@gmail.com>
2021-01-30 15:40:23 -0800

    relevant cde first

--- StableMarriage.cs
+++ StableMarriage.cs
@@ -2,7 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 namespace StableMarriage {
+    public class Program {
+        static public void StableMarriage(Dictionary<int, Man> men, Dictionary<int, Woman> women) {
+            List<int> availMen = men.Keys.ToList();
+            Dictionary<int, Match> matches = new Dictionary<int, Match>();
+            while (availMen.Any()) {
+                var m = men[availMen.First()];
+                bool manIsProposing = true;
+                while (manIsProposing) {
+                    var w = women[m.getNextWomenToProposeTo()];
+                    m.proposedTo.Add(w.id);
+                    if (w.wouldSwitchFor(m.id)) {
+                        if (matches.ContainsKey(w.id))
+                            matches.Remove(w.id);
+                        int oldMan = w.divorceAndMarry(m.id);
+                        if (oldMan != -1) availMen.Add(oldMan);
+                        availMen.Remove(m.id);
+                        matches.Add(w.id, new Match { man = m, women = w });
+                        manIsProposing = false;
+                    }
+                }
+            }
+            int i = 0;
+            foreach (var k in matches.Keys)
+                Console.WriteLine($"Match #{++i} : {matches[k].ToString()} \n");
+        }        
 
+        static void Main(string[] args) {
+            Dictionary<int, Man> men = new Dictionary<int, Man> {
+                { 0, new Man(0 , new List<int>{ 0 , 3 , 2 , 1 , 4 }) },
+                { 1, new Man(1 , new List<int>{ 0 , 3 , 2 , 4 , 1 }) },
+                { 2, new Man(2 , new List<int>{ 3 , 4 , 1 , 2 , 0 }) },
+                { 3, new Man(3 , new List<int>{ 2 , 1 , 4 , 3 , 0 }) },
+                { 4, new Man(4 , new List<int>{ 4 , 2 , 3 , 1 , 0  }) },
+            };
+
+            var women = new Dictionary<int, Woman> {
+                { 0, new Woman(0 , new List<int>{ 1 , 4 , 3 , 2 , 0 }) },
+                { 1, new Woman(1 , new List<int>{ 1 , 3 , 2 , 4 , 0 }) },
+                { 2, new Woman(2 , new List<int>{ 2 , 1 , 3 , 4 , 0 }) },
+                { 3, new Woman(3 , new List<int>{ 0 , 3 , 2 , 4 , 1 }) },
+                { 4, new Woman(4 , new List<int>{ 0 , 2 , 3 , 4 , 1 }) },
+            };
+            StableMarriage(men, women);
+        }
+    }
+    
     public class Person {
         public int id;
         public List<int> preference;
@@ -75,53 +120,6 @@ namespace StableMarriage {
                         + $"Women:{women.id} Rank:{getRanking(isWoman: true)}, "
                         + $"Deal={bestDeal()}";
             return output;
-        }
-    }
-
-    public class Program {
-
-        static void Main(string[] args) {
-            Dictionary<int, Man> men = new Dictionary<int, Man> {
-                { 0, new Man(0 , new List<int>{ 0 , 3 , 2 , 1 , 4 }) },
-                { 1, new Man(1 , new List<int>{ 0 , 3 , 2 , 4 , 1 }) },
-                { 2, new Man(2 , new List<int>{ 3 , 4 , 1 , 2 , 0 }) },
-                { 3, new Man(3 , new List<int>{ 2 , 1 , 4 , 3 , 0 }) },
-                { 4, new Man(4 , new List<int>{ 4 , 2 , 3 , 1 , 0  }) },
-            };
-
-            var women = new Dictionary<int, Woman> {
-                { 0, new Woman(0 , new List<int>{ 1 , 4 , 3 , 2 , 0 }) },
-                { 1, new Woman(1 , new List<int>{ 1 , 3 , 2 , 4 , 0 }) },
-                { 2, new Woman(2 , new List<int>{ 2 , 1 , 3 , 4 , 0 }) },
-                { 3, new Woman(3 , new List<int>{ 0 , 3 , 2 , 4 , 1 }) },
-                { 4, new Woman(4 , new List<int>{ 0 , 2 , 3 , 4 , 1 }) },
-            };
-            StableMarriage(men, women);
-        }
-
-        static public void StableMarriage(Dictionary<int, Man> men, Dictionary<int, Woman> women) {
-            List<int> availMen = men.Keys.ToList();
-            Dictionary<int, Match> matches = new Dictionary<int, Match>();
-            while (availMen.Any()) {
-                var m = men[availMen.First()];
-                bool manIsProposing = true;
-                while (manIsProposing) {
-                    var w = women[m.getNextWomenToProposeTo()];
-                    m.proposedTo.Add(w.id);
-                    if (w.wouldSwitchFor(m.id)) {
-                        if (matches.ContainsKey(w.id))
-                            matches.Remove(w.id);
-                        int oldMan = w.divorceAndMarry(m.id);
-                        if (oldMan != -1) availMen.Add(oldMan);
-                        availMen.Remove(m.id);
-                        matches.Add(w.id, new Match { man = m, women = w });
-                        manIsProposing = false;
-                    }
-                }
-            }
-            int i = 0;
-            foreach (var k in matches.Keys)
-                Console.WriteLine($"Match #{++i} : {matches[k].ToString()} \n");
         }
     }
 }
